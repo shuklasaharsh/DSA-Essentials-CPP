9c954a4d0d302c26b0b8aec719a46aff4c19ca2f
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2022-10-17 22:09:17 +0530

    feat: binary search

--- arrays.h
+++ arrays.h
@@ -45,4 +45,29 @@ int linear_search_index(const int *arr, int size, int key) {
     return -1;
 }
 
+/*
+ * binary_search searches in an array in a binary fashion and returns index if found
+ *
+ * @params: const int *arr | pointer to an array
+ *        :      int start | start of lookup
+ *        :      int end   | end of lookup
+ *        :       int key  | element that should be found
+ *
+ * @returns: int | index of the element if found, else returns -1
+ *
+ * Time Space : O(LOG(n))
+
+ */
+int binary_search(const int *arr, int start, int end, int key) {
+    int middle_index = (start + end)/2;
+
+    if (arr[middle_index] == key) {
+        return middle_index;
+    } else if (arr[middle_index] > key) {
+        return binary_search(arr, start, middle_index - 1, key);
+    } else {
+        return binary_search(arr, middle_index + 1, end, key);
+    }
+}
+
 #endif //DSA_ESSENTIALS_ARRAYS_H
