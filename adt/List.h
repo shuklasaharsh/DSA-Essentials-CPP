9797b62ae87d0949d04871ab91a0479712ba8e61
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2022-10-25 14:26:23 +0530

    feat: set debug

--- adt/List.h
+++ adt/List.h
@@ -13,13 +13,16 @@ const int DEFAULT_LIST_SIZE = 100;
 namespace adt {
     class List {
     private:
+        bool debug = false;
         int *arr;
         int size;
     public:
         friend bool reverse_cmp(int num1, int num2);
+
         // describes the number of elements in the list
         int length;
-        int &operator[] (int);
+
+        int &operator[](int);
 
         /*
          * Creates a list of default size
@@ -54,17 +57,25 @@ namespace adt {
             this->length = size;
         }
 
+        void set_debug() {
+            this->debug = true;
+        }
+
+        [[nodiscard("get debug prop")]]bool get_debug() const {
+            return this->debug;
+        }
+
         void insert(int element) {
             if (this->length + 1 >= this->size) {
                 // If length after adding an element is greater than size, we need to create a new array
                 int *dynamic_array = new int(2 * this->size);
-                this->size = 2 * this->size;
                 // Copy the existing array into the new one;
-                for (int i = 0; i < this->size; i++) {
+                for (int i = 0; i < this->length; i++) {
                     dynamic_array[i] = this->arr[i];
                 }
                 // And then insert it on length + 1
                 dynamic_array[this->length + 1] = element;
+                this->size = 2 * this->size;
                 this->length += 1;
                 arr = dynamic_array;
             } else {
@@ -77,14 +88,14 @@ namespace adt {
             // first we check if length + 1 will be == size
             if (this->length + 1 >= this->size) {
                 int *dynamic_array = new int(2 * this->size);
-                this->size = 2 * this->size;
                 // Copy the existing array into the new one;
-                for (int i = 0; i < this->size; i++) {
+                for (int i = 0; i < this->length; i++) {
                     dynamic_array[i] = this->arr[i];
                 }
                 // And then insert it on length + 1
                 dynamic_array[this->length + 1] = element;
                 this->length += 1;
+                this->size = 2 * this->size;
                 arr = dynamic_array;
             }
             // If not we carry on
@@ -96,10 +107,10 @@ namespace adt {
             this->length++;
         }
 
-        void print(bool debug = false) {
-            if (debug) {
+        void print(bool debug_print = false) {
+            if (debug_print) {
                 for (auto i = 0; i < this->length; i++) {
-                    std::cout << i <<"|"<< this->arr[i] << endl;
+                    std::cout << i << "|" << this->arr[i] << endl;
                 }
                 cout << endl;
             } else {
@@ -124,7 +135,7 @@ namespace adt {
             if (reverse) {
                 std::sort(arr, arr + length, std::greater<int>());
             } else {
-                std::sort(arr, arr+length, std::less<int>());
+                std::sort(arr, arr + length, std::less<int>());
             }
         }
     };
@@ -137,10 +148,17 @@ namespace adt {
         return this->arr[a];
     }
 
-    ostream &operator<<(ostream &o, adt::List l) {
-        for (int i = 0; i < l.length; i++) {
-            o << l[i] << " ";
+    ostream &operator<<(ostream &o, List l) {
+        if (l.get_debug()) {
+            for (int i = 0; i < l.length; i++) {
+                o << i << "|" << l[i] << endl;
+            }
+        } else {
+            for (int i = 0; i < l.length; i++) {
+                o << l[i] << " ";
+            }
         }
+
         return o;
     }
 
