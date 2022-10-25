5a386cb8c0d3864c3f6e2c67b06abe1649e4d899
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2022-10-25 11:49:41 +0530

    feat: insert with position

--- adt/List.h
+++ adt/List.h
@@ -18,6 +18,7 @@ namespace adt {
     public:
         // describes the number of elements in the list
         int length;
+
         /*
          * Creates a list of default size
          */
@@ -54,7 +55,8 @@ namespace adt {
         void insert(int element) {
             if (this->length + 1 >= this->size) {
                 // If length after adding an element is greater than size, we need to create a new array
-                int *dynamic_array = new int(2 * size);
+                int *dynamic_array = new int(2 * this->size);
+                this->size = 2 * this->size;
                 // Copy the existing array into the new one;
                 for (int i = 0; i < this->size; i++) {
                     dynamic_array[i] = this->arr[i];
@@ -67,6 +69,29 @@ namespace adt {
                 this->length += 1;
                 arr[length] = element;
             }
+        }
+
+        void insert(int element, int position) {
+            // first we check if length + 1 will be == size
+            if (this->length + 1 >= this->size) {
+                int *dynamic_array = new int(2 * this->size);
+                this->size = 2 * this->size;
+                // Copy the existing array into the new one;
+                for (int i = 0; i < this->size; i++) {
+                    dynamic_array[i] = this->arr[i];
+                }
+                // And then insert it on length + 1
+                dynamic_array[this->length + 1] = element;
+                this->length += 1;
+                arr = dynamic_array;
+            }
+            // If not we carry on
+            // To do this operation we shift the array by 1 pos
+            for (auto i = position; i < this-> length; i++) {
+                arr[i + 1] = arr[i];
+            }
+            arr[position] = element;
+            this->length++;
         }
 
         void print() {
