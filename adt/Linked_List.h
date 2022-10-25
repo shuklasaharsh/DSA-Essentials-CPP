4936c61caaad44b27e4117bc9650e29d0b5a8f06
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2022-10-25 21:14:48 +0530

    chore: cout

--- adt/Linked_List.h
+++ adt/Linked_List.h
@@ -5,25 +5,47 @@
 #ifndef DSA_ESSENTIALS_LINKED_LIST_H
 #define DSA_ESSENTIALS_LINKED_LIST_H
 
+#include "iostream"
+
 namespace adt {
     class Linked_List {
     private:
         int length = 0;
         struct Node {
             int data;
-            Node* next;
+            Node *next;
         };
-        Node* head{};
+        Node *head{};
     public:
-        Linked_List() = default;
+        friend std::ostream &operator<<(std::ostream &o, Linked_List l);
 
-        void insert(int data) {
-
+        Linked_List() {
+            auto d = new Node();
+            d->data = 0;
+            head = d;
         }
 
-    };
-}
+        auto get_head() const {return head;}
 
+        void insert(int data) {
+            auto d = new Node();
+            d->data = data;
+            head->next = d;
+        }
+    };
+
+    std::ostream &operator<<(std::ostream &o, Linked_List l) {
+        auto i = l.get_head();
+        while (i != nullptr) {
+            o << i->data << " ";
+            i = i->next;
+        }
+        o << std::endl;
+        return o;
+    }
+
+
+}
 
 
 #endif //DSA_ESSENTIALS_LINKED_LIST_H
