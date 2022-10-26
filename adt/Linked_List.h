1d0b3ca5107055c5cc3af6b957971b3cfee535f3
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2022-10-26 10:13:36 +0530

    feat: linked list

--- adt/Linked_List.h
+++ adt/Linked_List.h
@@ -8,40 +8,122 @@
 #include "iostream"
 
 namespace adt {
-    class Linked_List {
-    private:
-        int length = 0;
-        struct Node {
-            int data;
-            Node *next;
-        };
-        Node *head{};
+    class Node {
     public:
-        friend std::ostream &operator<<(std::ostream &o, Linked_List l);
+        int data;
+        Node *next;
 
-        Linked_List() {
-            auto d = new Node();
-            d->data = 0;
-            head = d;
+        Node() {
+            this->data = 0;
+            this->next = nullptr;
         }
 
-        [[nodiscard]] auto get_head() const {return head;}
-
-        void insert(int data) {
-            auto d = new Node();
-            d->data = data;
-            head->next = d;
+        explicit Node(int data) {
+            this->data = data;
+            this->next = nullptr;
         }
     };
 
-    std::ostream &operator<<(std::ostream &o, Linked_List l) {
-        auto i = l.get_head();
-        while (i != nullptr) {
-            o << i->data << " ";
-            i = i->next;
+    class LinkedList {
+        Node *head;
+    public:
+        LinkedList() { head = nullptr; }
+
+        void insert_node(int);
+
+        void print();
+
+        void delete_node(int);
+
+    };
+
+    void LinkedList::delete_node(int nodeOffset) {
+        Node *temp1 = head, *temp2 = nullptr;
+        int ListLen = 0;
+
+        if (head == nullptr) {
+            std::cout << "List empty." << std::endl;
+            return;
         }
-        o << std::endl;
-        return o;
+        while (temp1 != nullptr) {
+            temp1 = temp1->next;
+            ListLen++;
+        }
+        if (ListLen < nodeOffset) {
+            std::cout << "Index out of range"
+                      << std::endl;
+            return;
+        }
+
+        // Declare temp1
+        temp1 = head;
+
+        // Deleting the head.
+        if (nodeOffset == 1) {
+
+            // Update head
+            head = head->next;
+            delete temp1;
+            return;
+        }
+
+        // Traverse the list to
+        // find the node to be deleted.
+        while (nodeOffset-- > 1) {
+
+            // Update temp2
+            temp2 = temp1;
+
+            // Update temp1
+            temp1 = temp1->next;
+        }
+
+        // Change the next pointer
+        // of the previous node.
+        temp2->next = temp1->next;
+
+        // Delete the node
+        delete temp1;
+    }
+
+    void LinkedList::insert_node(int data) {
+        // Create the new Node.
+        Node *newNode = new Node(data);
+
+        // Assign to head
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        // Traverse till end of list
+        Node *temp = head;
+        while (temp->next != nullptr) {
+
+            // Update temp
+            temp = temp->next;
+        }
+
+        // Insert at the last.
+        temp->next = newNode;
+    }
+
+// Function to print the
+// nodes of the linked list.
+    void LinkedList::print() {
+        Node *temp = head;
+
+        // Check for empty list.
+        if (head == nullptr) {
+            std::cout << "List empty" << std::endl;
+            return;
+        }
+
+        // Traverse the list.
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
     }
 
 
